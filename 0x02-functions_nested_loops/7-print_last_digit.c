3538b6d9689c4bb43add88943ec3cf71ff96b0cc
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-12 22:06:52 +0300

    Write a function that prints the last digit of a number.
        Prototype: int print_last_digit(int);
        Returns the value of the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,26 +1,31 @@
 #include "main.h"
+#include "6-abs.c"
+#include <stdio.h>
 /**
  * print_last_digit - Prints the last digi of a number
  *
  * @n: the number to be striped off its last digit
  *
  *
- * Retrun: retursn the last digit of n;
+ * Return: retursn the last digit of n;
  *
  */
 
 int print_last_digit(int n)
 {
+
+	int r;
+
 	if (n < 0)
 	{
-		n *= -1;
-		_putchar ((n % 10) + '0');
-		return (n % 10);
+		r = n % 10;
+		r *= -1;
+		_putchar (r + '0');
+		return (r);
 	}
 	else
-	{
-	         _putchar ((n % 10) + '0');
-		 return (n % 10);
+	{	_putchar ((n % 10) + '0');
+		return (n % 10);
 
 	}
 
