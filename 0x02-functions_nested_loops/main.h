8e996974d799acb3dc31971394fad541fa2ed0c7
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-12 12:49:47 +0300

    add main.h file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*
+* _putchar - prints one character to screen
+*
+*
+* Return: returns 1 on succes and -1 on error
+*
+*/
+int _putchar(char c);
+
+#endif /* MAIN_H */
