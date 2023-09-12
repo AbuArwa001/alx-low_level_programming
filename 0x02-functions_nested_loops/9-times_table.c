2b010c4ea1f7b84db6647fd2597993b7f8a37b62
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-13 00:51:55 +0300

    Write a function that prints the 9 times table, starting with 0.
        Prototype: void times_table(void);
        Format: see example

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,47 @@
 #include "main.h"
-
+/**
+* procces - procces a times table
+* @p: product of i and j
+* @i: column of the table
+* @j: row of the table
+*/
+void procces(int p, int i, int j)
+{
+	for (j = 0; j <= 9; j++)
+	{	p = i * j;
+		if (p > 9)
+		{	_putchar((p / 10) + '0');
+			_putchar((p % 10)  + '0');
+			if (j == 9)
+			{	_putchar('\n');
+				continue;
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+		{	_putchar(p + '0');
+			if (j == 9)
+			{	_putchar('\n');
+				continue;
+			}
+			if (p == 0 || (p == 8 && i == 1))
+			{	_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				continue;
+			}
+			if (i >= 5 || p >= 8)
+			{	_putchar(',');
+				_putchar(' ');
+				continue;
+			}
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+	}
+}
 /**
  * times_table - prints 9 times table
  *
@@ -10,43 +52,9 @@
  */
 
 void times_table(void)
-{
-	int i = 0, j, p;
+{	int i = 0, j = 0, p = 0;
 
 	for (; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{	p = i * j;
-
-			if (p >= 10)
-			{	_putchar((p / 10) + '0');
-				_putchar((p % 10)  + '0');
-				if (j == 9)
-				{
-					_putchar('\n');
-					continue;
-				}
-
-				_putchar(',');
-				_putchar(' ');
-
-			}
-			else
-			{
-				_putchar(p + '0');
-
-				if (j == 9)
-				{	_putchar('\n');
-					continue;
-				}
-
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-
-			}
-
-
-		}
+	{	procces(p, i, j);
 	}
 }
