335c9d1172ba68c4156800a706eeecb7d45f0151
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-12 19:09:05 +0300

    remove bug

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,21 @@
 #include "main.h"
-/**                                                               * _islower - Write a function that checks for lowercase charac 
-  *       Prototype: int _islower(int c)
-  *               Returns 1 if c is lowercase
-  * Returns: returns 0  otherwise
-  *          returns 1 if c is lowercase
-  *
-  */
+/**
+ * _islower - Write a function that checks for lowercase charac
+ *       Prototype: int _islower(int c)
+ *@c: a character to be tested
+ *
+ *             Returns 1 if c is lowercase
+ * Return: returns 0  otherwise
+ *          returns 1 if c is lowercase
+ *
+ */
 int _islower(int c)
 {
 	char ch = 'a';
 
-	for (; c <='z'; c++)
+	for (; c <= 'z'; c++)
 	{
-		if (c == ch )
+		if (c == ch)
 		{
 			return (0);
 		}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
-*
 * _putchar - prints one character to screen
-*
+*@c: charcter to be printed
 *
 * Return: returns 1 on succes and -1 on error
 *
@@ -21,4 +20,14 @@ void print_alphabet(void);
 *
 */
 void print_alphabet_x10(void);
+/**
+* _islower - Write a function that checks for lowercase characte
+* @c: character to be tested
+*               Prototype: int _islower(int c);
+*               Returns 1 if c is lowercase
+* Return: returns 0  otherwise
+*          returns 1 if c is lowercase
+*
+*/
+int _islower(int c);
 #endif /* MAIN_H */
