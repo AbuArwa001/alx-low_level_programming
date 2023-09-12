007b119f42217003ecd7712095aace7aa954723d
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-12 22:43:57 +0300

    Write a function that prints every minute of the day of Jack Bauer
    starting from 00:00 to 23:59.
    
        Prototype: void jack_bauer(void);

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,13 +12,12 @@ void jack_bauer(void)
 	for (; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
-		{
-			_putchar((h / 10)  + '0');
+		{	_putchar((h / 10)  + '0');
 			_putchar((h % 10) + '0');
 			_putchar(':');
 			_putchar((m / 10) + '0');
 			_putchar((m % 10) + '0');
-		        _putchar('\n');
+			_putchar('\n');
 
 		}
 	}
