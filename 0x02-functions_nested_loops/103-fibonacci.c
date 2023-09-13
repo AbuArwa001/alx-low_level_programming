bb666e7d2d6d8dc5dc9085b19a58ef6de3e60d19
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-13 03:32:43 +0300

    Print fibonacci sum for values that do not excced 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints sum whoes values dont exceed 4m
+ *
+ *
+ * Return: always retunr 0
+ *
+ **/
+int main(void)
+{	unsigned long int sum = 2, p = 1, cur = 2, nxt = 0, tmp = 0;
+
+	while (nxt <= 4000000)
+	{	nxt = p + cur;
+		if ( nxt % 2 == 0)
+		{
+			sum += sum;
+
+		}
+		tmp = cur;
+		cur = nxt;
+		p = tmp;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
