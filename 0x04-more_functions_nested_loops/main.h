f1cb186c8881e27bbe1ea9938b7aebff81cac710
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-14 15:21:49 +0300

    Prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,15 @@
 #ifndef MAIN_H_
 #define MAIN_H_
-
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-
+void print_numbers(void);
 #endif /*MAIN_H_*/
