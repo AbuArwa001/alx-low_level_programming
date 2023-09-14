811297cef9b48d3555fb8843e42b752fee8247e5
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-14 15:41:36 +0300

    Print from 0-9 and Omit 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *  print_numbers - print numbers from 0 -9
+ *
+ */
+
+void print_most_numbers(void)
+{
+
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+
+		if (i == 2 || i == 4)
+		{
+
+			continue;
+		}
+
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
