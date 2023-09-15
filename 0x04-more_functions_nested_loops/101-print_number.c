6065017327ce3464118a9ea1205342298cbc001d
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-15 03:11:46 +0300

    Print number using _putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,48 +1,19 @@
 #include "main.h"
+
 /**
  * print_number - prints integers using _putchar
- *               it checks the size of the digit then  prints
  * @n: is the number to be printed
  */
 void print_number(int n)
-{	int tmp;
+{
 
-	if (n == 0)
-	{	_putchar(n + '0');
-
+	if (n < 0)
+	{	_putchar('-');
+		n = -n;
 	}
 
-	else
-	{
+	if (n / 10 != 0)
+		print_number(n / 10);
 
-		if (n < 0)
-		{	n *= -1;
-			_putchar('-');
-		}
-
-		if (n < 10)
-		{	_putchar(n + '0');
-		}
-
-		else if (n >= 10 && n <= 99)
-		{	_putchar((n / 10)  + '0');
-			_putchar((n % 10) + '0');
-		}
-
-		else if (n >= 100 && n <= 999)
-		{	tmp = n / 10;
-			_putchar((n / 100) + '0');
-			_putchar((tmp % 10) + '0');
-			_putchar((n % 10) + '0');
-		}
-
-		else if (n >= 1000 && n <= 9999)
-		{
-
-			_putchar((n / 1000) + '0');
-			_putchar(((n / 100) % 10) + '0');
-			_putchar(((n % 100) / 10) + '0');
-			_putchar((n % 10) + '0');
-		}
-	}
+	_putchar(n % 10 + '0');
 }
