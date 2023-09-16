341607956707deae9c49dcf801d91aac7519df20
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-16 21:20:30 +0300

    Remove spaces at the beggining

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,12 +10,15 @@ void print_times_row(int row, int n);
 void print_times_table(int n)
 {
 	int i = 0;
-	if (n >= 15 || n <= 0) {
+
+	if (n >= 15 || n <= 0)
+	{
 
 		return;
 	}
 
-	for (i = 0; i <= n; i++) {
+	for (i = 0; i <= n; i++)
+	{
 		print_times_row(i, n);
 	}
 }
@@ -28,16 +31,20 @@ void print_times_table(int n)
 void print_times_row(int row, int n)
 {
 	int j = 0;
-	for (j = 0; j <= n; j++) {
+
+	for (j = 0; j <= n; j++)
+	{
 		print_times_cell(row * j);
-		if (row == 0 && j != n) {
+		if (row == 0 && j != n)
+		{
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
 			_putchar(' ');
 			continue;
 		}
-		if (j != n) {
+		if (j != n)
+		{
 			_putchar(',');
 			_putchar(' ');
 		}
@@ -51,20 +58,26 @@ void print_times_row(int row, int n)
  */
 void print_times_cell(int cell)
 {
-	if (cell <= 9) {
+	if (cell <= 9)
+	{
 		if (cell != 0)
 		{
-		_putchar(' ');
-		_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
 		}
 		_putchar(cell + '0');
-	} else if (cell <= 99) {
-		_putchar(' ');
-		_putchar((cell / 10) + '0');
-		_putchar((cell % 10) + '0');
-	} else {
-		_putchar((cell / 100) + '0');
-		_putchar(((cell / 10) % 10) + '0');
-		_putchar((cell % 10) + '0');
 	}
+	else
+		if (cell <= 99)
+		{
+			_putchar(' ');
+			_putchar((cell / 10) + '0');
+			_putchar((cell % 10) + '0');
+		}
+		else
+		{
+			_putchar((cell / 100) + '0');
+			_putchar(((cell / 10) % 10) + '0');
+			_putchar((cell % 10) + '0');
+		}
 }
