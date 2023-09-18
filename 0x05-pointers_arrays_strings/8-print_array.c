bd5ddbc6d7723a13216d65264884bd3bf34dd8f7
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-18 23:03:21 +0300

    Print n number integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,10 +9,15 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
+    if (n <= 0)
+    {
+      return;
+    }
 	for (; i < n ; i++)
 	{
-		printf("%d, ", a[i]);
+		printf("%d", a[i]);
+        if ( i != (n - 1))
+            printf(", ");
 	}
-	printf("%d", a[i]);
 	printf("\n");
 }
