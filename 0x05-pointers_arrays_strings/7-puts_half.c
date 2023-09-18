2807d706cbd45e84cec2c7b308c2690d89cfde63
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-18 22:08:54 +0300

    Print half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * puts_half - prints half of the str
+ * @str: string to be sliced
+ */
+void puts_half(char *str)
+{
+	int len = _strlen(str), start = 0;
+
+	if ((len % 2) == 0)
+	{
+		start = len / 2;
+		while (*(str + start) != '\0')
+		{
+			_putchar(*(str + start));
+			start++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		start = (len - 1) / 2;
+
+		while (*(str + start) != '\0')
+		{
+			_putchar(*(str + start));
+			start++;
+		}
+		_putchar('\n');
+	}
+}
