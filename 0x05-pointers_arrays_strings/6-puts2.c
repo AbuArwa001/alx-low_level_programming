e717f9b15f9ad08cfb9165078c4b300797919123
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-18 21:39:57 +0300

    Puts some chars while ommitting others

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,8 +10,8 @@ void puts2(char *str)
 
 	while (*(str + i) != '\0')
 	{
-		if ((((int) *(str + i) + 0) % 2) == 0)
-			_putchar(((int) *(str + i) + 0));
+		if (i % 2 == 0)
+			_putchar(*(str + i));
 		i++;
 	}
 	_putchar('\n');
