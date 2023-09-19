72e1f560fa740c746c95e8d8641bdb612001160f
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-19 15:44:17 +0300

    Generate password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * getRandomLowercaseLetter- generate password or elf file
+ * Return: rreturns a random character;
+ */
+
+char getRandomLowercaseLetter(void)
+{
+	return ('a' + rand() % 26);
+}
+/**
+ * getRandomDigit - generate password or elf file
+ * Return: returns a random digit
+ */
+
+char getRandomDigit(void)
+{
+	return ('0' + rand() % 10);
+}
+/**
+ * generateRandomPassword  - generate Radom passwordfile
+ * @password: pasword to be generated;
+ * @length: length of the paswword
+ */
+
+void generateRandomPassword(char *password, int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		if (i % 2 == 0)
+		{
+			password[i] = getRandomLowercaseLetter();
+		}
+		else
+		{
+			password[i] = getRandomDigit();
+		}
+	}
+
+	password[length] = '\0';
+}
+/**
+ * main - generate password or elf file
+ * Return: Always returns 0;
+ */
+int main(void)
+{
+	srand(time(NULL));
+	int passwordLength = 12;
+	char password[passwordLength + 1];
+
+	generateRandomPassword(password, passwordLength);
+	printf("%s", password);
+
+	return (0);
+}
