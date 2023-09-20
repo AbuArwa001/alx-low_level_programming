e08b0202fb23dd866a8de039bb1526c6ff762053
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-20 15:31:56 +0300

    Capitalize after special charaters

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * cap_string - Capitalize beggining of a string
+ * @str: string to be capitalized
+ * Return: returns a capitalize string
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0, j = 0;
+	char *special = ",.;!?(){}\t\n ";
+
+	while (str[i] != '\0')
+	{
+		j = 0;
+
+		while (special[j] != '\0')
+		{
+			if (special[j] == str[i])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					/*printf("character is %c\n", str[i+1]);*/
+					str[i + 1] = str[i + 1] - 32;
+					/*str[i+1] = str[i + 1] - 32;*/
+				}
+			}
+
+			j++;
+		}
+
+		i++;
+	}
+
+	return (str);
+}
