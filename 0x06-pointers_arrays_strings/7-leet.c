d24e22b702d868e1ff09504c16bed9fd40fc84e7
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-20 23:53:39 +0300

    Perform leet using while loop

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include "2-strlen.c"
 /**
  * leet - encodes a string into 1337
  * @str: string to be incoded
@@ -7,17 +7,43 @@
  */
 char *leet(char *str)
 {
-	int i = 0;
+	int i = 0, k;
+	char *cipher = "AaEeOoTtLl";
 
 	while (str[i] != '\0')
-{str[i] = (str[i] == 'A' || str[i] == 'a') ? '4' :
-(str[i] == 'E' || str[i] == 'e') ? '3' :
-(str[i] == 'O' || str[i] == 'o') ? '0' :
-(str[i] == 'L' || str[i] == 'l') ? '1' :
-(str[i] == 'T' || str[i] == 't') ? '7' :
-str[i];
-i++;
-}
-
+	{
+		for (k = 0; k < _strlen(cipher); k++)
+		{
+			while (cipher[k] == str[i])
+			{
+				while ((k / 2) == 0)
+				{
+					str[i] = '4';
+					break;
+				}
+				while ((k / 2) == 1)
+				{
+					str[i] = '3';
+					break;
+				}
+				while ((k / 2) == 2)
+				{
+					str[i] = '0';
+					break;
+				}
+				while ((k / 2) == 3)
+				{
+					str[i] = '7';
+					break;
+				}
+				while ((k / 2) == 4)
+				{
+					str[i] = '1';
+					break;
+				}
+			}
+		}
+		i++;
+	}
 	return (str);
 }
