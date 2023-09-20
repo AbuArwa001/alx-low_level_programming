07f3521998fef19eeb1577947b4d207394bc263a
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-20 21:10:59 +0300

    SUbstitute array with string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,27 +7,28 @@
  */
 char *cap_string(char *str)
 {
-    int i = 0;
+	int i = 0;
 
 
-    if (str[0] >= 'a' && str[0] <= 'z')
-        str[0] -= 32;
-    while (str[i] != '\0')
-    {
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
 
-        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-            str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-            str[i] == '!' || str[i] == '?' || str[i] == '\"' ||
-            str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-            str[i] == '}')
-        {
+	while (str[i] != '\0')
+	{
 
-            if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-                str[i + 1] -= 32;
-        }
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+			str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+			str[i] == '!' || str[i] == '?' || str[i] == '\"' ||
+			str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+			str[i] == '}')
+		{
 
-        i++;
-    }
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] -= 32;
+		}
 
-    return (str);
+		i++;
+	}
+
+	return (str);
 }
