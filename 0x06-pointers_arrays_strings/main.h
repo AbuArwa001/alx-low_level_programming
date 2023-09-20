826a0af0c1edf68ddf92c68772dc1f32cde91c80
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-20 08:38:41 +0300

    Concatenate two strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H_
+#define MAIN_H
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+/**
+ * _strlen - computes the size of a string
+ *
+ * @s: the string to be examined
+ * Return: returns the length in number
+ */
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+#endif /* MAIN_H_*/
