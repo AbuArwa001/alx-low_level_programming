9f2058fdc5420bbd167ebc888a56776ebcd35032
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-21 00:06:23 +0300

    Encode using map

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,19 +7,24 @@
  */
 char *leet(char *str)
 {
-int i = 0;
-char leetMap[256] = {0};
+	int i = 0;
+	char leetMap[256] = {0};
 
-    leetMap['a'] = leetMap['A'] = '4';
-    leetMap['e'] = leetMap['E'] = '3';
-    leetMap['o'] = leetMap['O'] = '0';
-    leetMap['t'] = leetMap['T'] = '7';
-    leetMap['l'] = leetMap['L'] = '1';
-    while (str[i]) {
-        if (leetMap[(unsigned char)str[i]]) {
-            str[i] = leetMap[(unsigned char)str[i]];
-        }
-        i++;
-    }
+	leetMap['a'] = leetMap['A'] = '4';
+	leetMap['e'] = leetMap['E'] = '3';
+	leetMap['o'] = leetMap['O'] = '0';
+	leetMap['t'] = leetMap['T'] = '7';
+	leetMap['l'] = leetMap['L'] = '1';
+
+	while (str[i])
+	{
+		if (leetMap[(unsigned char)str[i]])
+		{
+			str[i] = leetMap[(unsigned char)str[i]];
+		}
+
+		i++;
+	}
+
 	return (str);
 }
