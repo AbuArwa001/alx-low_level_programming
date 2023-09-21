f98ca6407579d873fdc36e6dc813758727e0e870
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-21 20:09:34 +0300

    Prints a buffer of 10 chars per line

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,23 +1,6 @@
 #include "main.h"
 #include <string.h>
 #include <stdio.h>
-/**
- * _strlen - computes the size of a string
- *
- * @s: the string to be examined
- * Return: returns the length in number
- */
-int _strlen1(char *s)
-{
-	int length = 0, i = 0;
-
-	while (s[i] != '\0')
-	{
-		length++;
-        i++;
-	}
-	return (length);
-}
 
 /**
  * print_buffer - prints buffer
@@ -26,47 +9,42 @@ int _strlen1(char *s)
  */
 void print_buffer(char *b, int size)
 {
-	int i = 0, j = 0, l = 0, k = 0, m= 0;
+	int i = 0, j = 0, l = 0, k = 0, m = 0;
 
 	for (i = 0; i < size; i += 10)
 	{
-
 		printf("%08x: ", i);
-
 		for (j = 0; j < 10 ; j++)
 		{
-
-            if (j %2 == 0)
-            {
-                printf(" ");
-            }
-            if (m >= size) {
-                printf("  ");
-            }else {
-                printf("%02x", b[m]);
-            }
-            m++;
+			if (j % 2 == 0)
+			{
+				printf(" ");
+			}
+			if (m >= size)
+			{
+				printf("  ");
+			}
+			else
+			{
+				printf("%02x", b[m]);
+			}
+			m++;
 		}
-        printf(" ");
-
+		printf(" ");
 		for (l = 0; l < 10  && k < size; l++)
 		{
-            if (b[k] == '\0')
-            {
-			  printf(".");
-            }
-            else
-            {
-                if (b[k] == '\n' || (b[k] > 0 && b[k] < 8))
-                {
-                  printf(".");
-                }
-                else {
- 			printf("%c", b[k]);
-
-                }
-            }
-            k++;
+			if (b[k] == '\0')
+			{
+				printf(".");
+			}
+			else
+			{
+				if (b[k] == '\n' || (b[k] > 0 && b[k] < 8))
+					printf(".");
+				else
+					printf("%c", b[k]);
+			}
+			k++;
 		}
 		printf("\n");
 	}
