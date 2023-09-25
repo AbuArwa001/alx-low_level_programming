038dc4150151488238edc3ff6c9bab3f5f481541
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-25 16:54:31 +0300

    Locate a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strstr - Finds the first occurrence of the substrin needle in the
+ *           str haystack
+ * @haystack: strin to be scanned
+ * @needle: string to be seacrhed
+ * Return: returns the remaining from the needle found
+ *         returns null if not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0, k = 0;
+
+	while (haystack[i] != '\0')
+	{
+
+		k = i;
+		j = 0;
+
+		while (haystack[k] == needle[j] && needle[j] != '\0')
+		{
+			k += 1;
+			j++;
+		}
+
+		if (needle[j] != '\0' && j != 0)
+		{
+			return ("NULL");
+		}
+		else
+			if (j != 0 && needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
+
+		i++;
+
+	}
+
+	return ("NULL");
+}
