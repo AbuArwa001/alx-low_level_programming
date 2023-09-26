fc627ef4d498d79fc9bfd7bf156da3000fb4d741
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-26 03:52:51 +0300

    Print sum of diagnal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,21 +8,21 @@
  */
 void print_diagsums(int *a, int size)
 {
-    int row, col;
-    int sum_diag1 = 0;
-    int sum_diag2 = 0;
+	int row, col;
+	int sum_diag1 = 0;
+	int sum_diag2 = 0;
 
-    for (row = 0; row < size; row++)
-    {
-        for (col = 0; col < size; col++)
-        {
-            if (row == col)
-                sum_diag1 += a[row * size + col];
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+		{
+			if (row == col)
+				sum_diag1 += a[row * size + col];
 
-            if (row == size - col - 1)
-                sum_diag2 += a[row * size + col];
-        }
-    }
+			if (row == size - col - 1)
+				sum_diag2 += a[row * size + col];
+		}
+	}
 
-    printf("%d, %d\n", sum_diag1, sum_diag2);
+	printf("%d, %d\n", sum_diag1, sum_diag2);
 }
