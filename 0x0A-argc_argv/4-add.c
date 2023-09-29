465a977c5956eb31416b13950c524ceae9abf287
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-29 10:28:53 +0300

    Add arguments paase to command

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+#include "main.h"
+
+/**
+ * main - prints sum of  arguments
+ * @argc: arguments count
+ * @argv: argument vector
+ * Return: returns 0 on success and non zero on fail
+ */
+int main(int argc, char **argv)
+{
+	int sum = 0, i = 1;
+
+	if (argc > 1)
+	{
+		for (; i < argc ; i++)
+		{
+			if (!atoi(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+			sum += atoi(argv[i]);
+		}
+
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("%d\n", 0);
+	}
+
+
+	return (EXIT_SUCCESS);
+}
