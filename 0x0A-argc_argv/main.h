55f1d7d914936a0915843aaa3698a3d4e5b98108
AbuArwa001 <khalfanathman12@yahoo.com>
2023-09-29 08:34:22 +0300

    Add main and README files

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+
+#endif /* MAIN_H_*/
