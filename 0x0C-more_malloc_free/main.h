fc650b3b03bf53ef712789f2211813fca9805c5f
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-05 12:19:19 +0300

    Add strlen function

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,8 +2,14 @@
 #define MAIN_H_
 #include <stddef.h>
 #include <stdlib.h>
-
+/**
+ * _strlen - computes the size of a string
+ *
+ * @s: the string to be examined
+ * Return: returns the length in number
+ */
+int _strlen(char *s);
 void *malloc_checked(unsigned int b);
-
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif
