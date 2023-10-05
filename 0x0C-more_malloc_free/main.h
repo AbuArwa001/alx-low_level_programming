62428771ca2f57c9c8cb2e2a5e54e1b60ce24ae5
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-05 21:50:37 +0300

    Reallocate memory block using malloc and free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,17 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
+ * _memcpy - copies n bytes from memory area src to memory
+ *           area dest
+ * @dest: destination memory area of src
+ * @src: source memory area to be copued
+ * @n: number of bytes to be copied
+ *
+ * Return: returns dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+/**
  * _strlen - computes the size of a string
  *
  * @s: the string to be examined
@@ -12,4 +23,6 @@ int _strlen(char *s);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
