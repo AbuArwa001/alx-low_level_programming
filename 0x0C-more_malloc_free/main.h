914550940a983c82db6efd8d3885932ad9f123a5
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-05 22:08:19 +0300

    Add memset function to repo

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -14,6 +14,16 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 /**
+ * _memset - it sets the n bytes with  b pointed to by s
+ * @s: pointer to memory to be manipulated
+ * @b: charcter to be used to manipulate
+ * @n: number of bytes to use
+ *
+ * Return: it returns a pointer to s;
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+/**
  * _strlen - computes the size of a string
  *
  * @s: the string to be examined
