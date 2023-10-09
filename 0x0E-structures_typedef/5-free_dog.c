deecbda15a427be7abfb12c540cfe6fedabc20a5
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-10 00:23:43 +0300

    Point struct to NULL

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,11 +1,18 @@
 #include "dog.h"
-
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
 /**
  * free_dog -  frees memowry allocated to dog struct
  * @d: dog struct to be freed
  */
+
 void free_dog(dog_t *d)
 {
 	if (d != NULL)
+    {
 		free(d);
+        d = NULL;
+    }
+    return;
 }
