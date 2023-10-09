bac4a1abe8f567092518f82263a159afed86b4b4
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-09 20:39:29 +0300

    Add conditionals

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 			d->name = "(nil)";
 		if (d->owner)
 			d->owner = "(nil)";
-		if (!d->age)
+		if (d->age != d->age)
 			printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
 		else
 			printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
