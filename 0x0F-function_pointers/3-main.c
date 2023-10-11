f90e11e6a3c8f8aa38140eddc7669be71f732c3e
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-12 01:02:59 +0300

    Check for vals other than ints

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,19 +18,24 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
+
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	if (get_op_func(argv[2]) == NULL)
+
+	if (get_op_func(argv[2]) != NULL && get_op_func(argv[2])(num1, num2))
+	{
+
+		result =  get_op_func(argv[2])(num1, num2);
+		printf("%d\n", result);
+	}
+	else
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	result =  get_op_func(argv[2])(num1, num2);
-	printf("%d\n", result);
 
 	return (0);
 }
