c77c3d680b223c52881af3fd08326f9b9306a232
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-13 02:20:22 +0300

    Reformat files header and print

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,40 +7,42 @@
  * @ch: format string
  * @args: va_slist
  */
-void print(char c, char *ch, va_list args)
-{
-	char *cha, charac;
-	float f = 0;
-
-	switch (c)
-	{
-		case 'c':
-			charac = va_arg(args, int);
-			printf(ch, charac);
-			break;
-
-		case 'd':
-			printf(ch, va_arg(args, int));
-			break;
-
-		case 'f':
-			f = va_arg(args, int);
-			printf(ch, f);
-			break;
-
-		case 's':
-			cha = va_arg(args, char *);
-
-			if (cha == NULL)
-				cha = "(nil)";
-
-			printf(ch, cha);
-			break;
-
-		default:
-			break;
-	}
-}
+/**
+*void print(char c, char *ch, va_list args)
+*{
+*	char *cha, charac;
+*	float f = 0;
+*
+*	switch (c)
+*	{
+*		case 'c':
+*			charac = va_arg(args, int);
+*			printf(ch, charac);
+*			break;
+*
+*		case 'd':
+*			printf(ch, va_arg(args, int));
+*			break;
+*
+*		case 'f':
+*			f = va_arg(args, int);
+*			printf(ch, f);
+*			break;
+*
+*		case 's':
+*			cha = va_arg(args, char *);
+*
+*			if (cha == NULL)
+*				cha = "(nil)";
+*
+*			printf(ch, cha);
+*			break;
+*
+*		default:
+*			break;
+*	}
+*}
+*/
 /**
  * print_all - prints anything
  * @format: a list of types of arguments
@@ -71,7 +73,7 @@ void print_all(const char *const format, ...)
 				ch[0] = '%';
 				ch[1] = '\0';
 				_strcat(ch, fm[j].chara);
-				print(fm[j].chara[0], ch, args);
+				/*print(fm[j].chara[0], ch, args);*/
 
 				if (format[i + 1] != '\0')
 					printf(", ");
