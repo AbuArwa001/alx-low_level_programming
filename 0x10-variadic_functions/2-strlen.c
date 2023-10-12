9d35c17d1e7d7f510f8bfa635582713f41a1fd5a
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-13 02:02:13 +0300

    Print anything

---
+++ 0x10-variadic_functions/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ * _strlen - computes the size of a string
+ *
+ * @s: the string to be examined
+ * Return: returns the length in number
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+	{
+		length++;
+	}
+	return (length);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,9 +2,38 @@
 #define VARIADIC_FUNCTIONS_H_
 #include <stdarg.h>
 #include <stddef.h>
+/**
+ * struct fmt - formart str
+ * @c: name of the dog
+ * @chara: variable type can be (char, int)
+ *
+ * Description: Longer description
+ */
+struct fmt
+{
+        char c;
+        char *chara;
+};
+
+typedef struct fmt fmt;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+/**
+ * _strlen - computes the size of a string
+ *
+ * @s: the string to be examined
+ * Return: returns the length in number
+ */
+int _strlen(char *s);
+/**
+ * _strcat - it concantenates two strings
+ * @dest: string to be appended to
+ * @src: string to be appended from
+ * Return: retursns combined strins
+ */
 
+char *_strcat(char *dest, char *src);
 #endif
