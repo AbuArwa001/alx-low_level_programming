d6fbefa4fa3c888607c7506f87a3b7e667679b42
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-13 04:42:41 +0300

    Remove check function

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,22 +2,6 @@
 #include "0-strcat.c"
 #include <stdio.h>
 /**
- * st_chk - checks string if null
- * @c: a format specifier if string
- * @st: string to be checked
- * Return: returns strin or (nil) if NUUL
- */
-
-char *st_chk(char c, char *st)
-{
-	if (c == 's' && st == NULL)
-	{
-		st = "(nil)";
-	}
-
-	return (st);
-}
-/**
  * print_all - prints anything
  * @format: a list of types of arguments
  */
@@ -48,7 +32,7 @@ void print_all(const char *const format, ...)
 				ch[1] = '\0';
 				_strcat(ch, fm[j].chara);
 				cha = va_arg(args, char *);
-				cha = st_chk(fm[j].chara[0], cha);
+
 				printf(ch, cha);
 				/*print(fm[j].chara[0], ch, args);*/
 				if (format[i + 1] != '\0')
