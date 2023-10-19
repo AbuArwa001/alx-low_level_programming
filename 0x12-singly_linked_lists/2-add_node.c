bc07d30f26f54f655891b4806d9fc57d3223323e
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-19 23:39:52 +0300

    Consider null strings

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,6 +10,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	char *strd = strdup(str);
+	int len = 0;
 	list_t *ptr = malloc(sizeof(list_t));
 
 	if (ptr == NULL)
@@ -17,8 +18,15 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
+	if (strd == NULL)
+		len = 0;
+	else
+	{
+		len = _strlen(strd);
+	}
+
 	ptr->str = strd;
-	ptr->len = _strlen(strd);
+	ptr->len = len;
 	ptr->next = NULL;
 
 	ptr->next = *head;
