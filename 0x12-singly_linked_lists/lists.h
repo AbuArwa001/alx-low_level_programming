cef3f7292427763b035c438663e896ce2fb1de54
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-19 22:33:25 +0300

    format according to betty

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -11,16 +11,16 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 /**
  * print_list - prints contents of the list
  * @h: poiter to the first node
- * 
+ *
  * Return: returns number of nodes in the lis
- * 
+ *
 */
 size_t print_list(const list_t *h);
 #endif
