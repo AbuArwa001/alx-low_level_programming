bfb659ee6480f56ca4887cdd8ac307c25d01b06b
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-20 04:58:00 +0300

    Free the list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_list - frees every node in the list
+ * @head: list head
+*/
+void free_list(list_t *head)
+{
+    list_t *temp;
+
+    while (head != NULL)
+    {
+        temp = &head; 
+        head = &head->next;  
+        free(temp);  
+    }
+}
