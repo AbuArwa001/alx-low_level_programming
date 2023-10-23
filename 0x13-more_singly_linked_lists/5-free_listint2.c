b138deeade0099a45c4a90f77ef797e0921467cf
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-23 12:27:26 +0300

    Free the list using its address

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees list and sets head to null
+ * @head: is the head/beginning of the list
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *h = *head;
+
+	while (h != NULL)
+	{
+		temp = h;
+		h = h->next;
+		free(temp);
+	}
+
+	*head = NULL;
+}
