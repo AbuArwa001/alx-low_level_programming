31c9ce91f27df32b4aa9ceb4e5c86da42ba3df25
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-24 10:24:54 +0300

    Free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,18 +5,24 @@
 */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp, *h = *head;
+	listint_t *temp;
 
 	if (*head == NULL)
 	{
 		return;
 	}
 
-	while (h != NULL)
+	while (*head != NULL)
 	{
-		temp = h;
-		h = h->next;
+		temp = *head;
+		*head = (*head)->next;
 		free(temp);
+
+		if (temp == *head)
+		{
+			break;
+		}
+
 	}
 
 	*head = NULL;
