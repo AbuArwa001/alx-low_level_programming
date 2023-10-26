0883f47d2fa888966be0764441399718b3a0e2cd
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-26 04:28:45 +0300

    Create a node first

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,6 +13,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int ptr = 0;
 	listint_t *new_node = malloc(sizeof(listint_t));
 
+	new_node->n = n;
+	new_node->next = NULL;
+
 	if (new_node == NULL)
 		return (NULL);
 
