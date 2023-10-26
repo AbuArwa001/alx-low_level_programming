304e1a195e2e8bb4ca99da6bdb2092e832d0a789
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-27 00:48:23 +0300

    Get nth bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * get_bit - returns the value of a bit ata given index
+ * @index: index of a bit in n
+ * @n: number to serch s bit
+ * Return: returns the value os a bit of a given index
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int _int = n;
+
+
+	if (index < 0)
+	{
+		return (-1);
+	}
+
+	if ((1 << index) <= _int)
+		return ((n & (1 << index) ? 1 : 0));
+	else
+		return (-1);
+}
