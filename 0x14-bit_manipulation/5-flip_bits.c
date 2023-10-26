5285a6ab9df88c069a867c214da8a16e0b3c98ff
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-27 02:34:25 +0300

    Check number of bits to flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include "2-get_bit.c"
+#include "3-set_bit.c"
+#include "4-clear_bit.c"
+
+/**
+ * flip_bits - counts number of bits to be flipe for nums to be equal
+ * @n: num1
+ * @m: num2
+ * Return: returns number of bits to be fliped
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int  i = 0, idx1 = 0, idx2 = 0, count = 0;
+
+
+	while (i < 32)
+	{
+		idx1 = get_bit(n, i);
+		idx2 = get_bit(m, i);
+
+		if (idx1 != idx2)
+		{
+			if (idx2 == 0)
+			{
+				clear_bit(&n, i);
+			}
+			else
+			{
+				set_bit(&n, i);
+			}
+
+			count++;
+		}
+
+		i++;
+	}
+
+	return (count);
+}
