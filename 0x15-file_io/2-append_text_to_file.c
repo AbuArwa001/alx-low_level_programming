28de0aa4edbc42dd2716cbf3fc9fefd92ea4e9f1
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-30 22:53:46 +0300

    Add errno condition

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			close(fd);
 			return (1);
 		}
-		else
+		if (errno == EACCES || errno == ENOENT)
 		{
 			close(fd);
 			return (-1);
