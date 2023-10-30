916dec3261c4dec4976732974faaa1ceb0c1aaae
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-31 02:52:34 +0300

    Free buffer

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,8 +7,9 @@
  * closing - it closes file discriptors
  * @fd1: file discriptor1
  * @fd2: ffile dicriptor 2
+ * @buf: buffer to be freed
 */
-void closing(int fd1, int fd2)
+void closing(int fd1, int fd2, int **buf)
 {
 	int  cl1 = 0, cl2 = 0;
 
@@ -27,6 +28,8 @@ void closing(int fd1, int fd2)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
+
+	free(*buf);
 }
 /**
  * main - a program that copies contet of a file to another
@@ -44,25 +47,25 @@ int main(int ac, char const *av[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
+	if (buff == NULL)
+	{
+		free(buff);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fd1);
+		exit(98);
+	}
 	fd1 = open(av[1], O_RDONLY);
-
 	if (fd1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
 	reaad = read(fd1, buff, 1024);
-
 	if (fd1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR
-| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-
+	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
@@ -74,6 +77,6 @@ int main(int ac, char const *av[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	closing(fd1, fd2);
+	closing(fd1, fd2, &buff);
 	return (0);
 }
