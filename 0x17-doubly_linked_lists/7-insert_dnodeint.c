8fb0deaec28ee53d4aedef269e24e3371fdd61e5
AbuArwa001 <khalfanathman12@yahoo.com>
2023-12-08 03:48:03 +0300

    insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -39,7 +39,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	else
 	{
-		return (NULL);
+		if (node == NULL && size == idx)
+		{
+			free(new_node);
+			return (add_dnodeint_end(h, n));
+		}
 	}
 
 	return (new_node);
