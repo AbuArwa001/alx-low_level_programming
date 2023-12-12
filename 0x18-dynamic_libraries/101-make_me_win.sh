30ede7f88a5131d045e02c31c474a5a300201a49
AbuArwa001 <khalfanathman12@yahoo.com>
2023-12-12 21:45:35 +0300

    Crete share library entry

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/AbuArwa001/alx-low_level_programming/blob/main/0x18-dynamic_libraries/ran.so
+wget  -O /tmp/ran.so https://github.com/AbuArwa001/alx-low_level_programming/blob/main/0x18-dynamic_libraries/ran.so
 export LD_PRELOAD=/tmp/ran.so
