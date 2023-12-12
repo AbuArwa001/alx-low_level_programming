c299ea8b6fd031068496e1d3fbe93ed236c180db
AbuArwa001 <khalfanathman12@yahoo.com>
2023-12-12 14:13:11 +0300

    Add executable

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/AbuArwa001/alx-low_level_programming/raw/master/0x18-dynamic_libraries/run.so
-export LD_PRELOAD=$PWD/run.so
+wget -P /tmp https://github.com/AbuArwa001/alx-low_level_programming/blob/main/0x18-dynamic_libraries/ran.so
+export LD_PRELOAD=/tmp/run.so
