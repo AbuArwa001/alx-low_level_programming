e47b97f89fb9800af002c99d47402b2b6da7d679
AbuArwa001 <khalfanathman12@yahoo.com>
2023-12-14 19:26:47 +0300

    Modify

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget  -O /tmp/ran.so https://github.com/AbuArwa001/alx-low_level_programming/blob/main/0x18-dynamic_libraries/ran.so
-export LD_PRELOAD=/tmp/ran.so
+wget  -O /tmp/random.so https://github.com/AbuArwa001/alx-low_level_programming/blob/main/0x18-dynamic_libraries/random.so
+export LD_PRELOAD=/tmp/random.so
