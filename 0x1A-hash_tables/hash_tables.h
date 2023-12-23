9b1086db1fd0c7648f4d9b399e008dc87f43a5f7
AbuArwa001 <khalfanathman12@yahoo.com>
2023-12-23 03:30:52 +0300

    Update insert and perform search

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -63,7 +63,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 /**
  * hash_table_get -  function that retrieves a value associated with a key
- * @ht is the hash table you want to look into
+ * @ht: is the hash table you want to look into
  * @key: is the key you are looking for
  * Return: returns the value for the key
 */
