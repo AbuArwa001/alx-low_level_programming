0deef74cf95de58e47378d6171e0e90b7b0c42e0
AbuArwa001 <khalfanathman12@yahoo.com>
2024-04-09 09:31:27 +0300

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 #include <math.h>
 /**
- * linear_search - search items in lineaar algorith
+ * binary_search - search items in lineaar algorith
  * @array: array to search from
  * @size: size of the array
  * @value: value to search
@@ -11,37 +11,44 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t i = 0, mid = 0, left = 0, right = 0;
+	size_t i = 0, mid = 0, left = 0, right = 0;
 
 	if (!array)
 		return (-1);
-    left = 0;
-    right = size - 1;
-    while (left <= right)
-    {
-        printf("Searching in array: ");
-        for (i = left; i <= right; i++)
-        {
-            if (i == right)
-            {
-                printf("%d", array[i]);
-            }
-            else
-            {
-                printf("%d, ", array[i]);
-            }
-            
-            
-        }
-        printf("\n");
-        
-        mid = floor((left + right) / 2);
-        if (array[mid] < value)
-            left = mid + 1;
-        else if (array[mid] > value)
-            right = mid - 1;
-        else
-            return mid;
-    }
-    return (-1);
+
+	left = 0;
+	right = size - 1;
+
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+
+		for (i = left; i <= right; i++)
+		{
+			if (i == right)
+			{
+				printf("%d", array[i]);
+			}
+			else
+			{
+				printf("%d, ", array[i]);
+			}
+
+
+		}
+
+		printf("\n");
+
+		mid = floor((left + right) / 2);
+
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			if (array[mid] > value)
+				right = mid - 1;
+			else
+				return (mid);
+	}
+
+	return (-1);
 }
